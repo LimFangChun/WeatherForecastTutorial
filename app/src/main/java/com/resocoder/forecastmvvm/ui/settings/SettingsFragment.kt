0f0ebfd29d56f45a8e22d24f366ad0e980da4f28
LimFangChun <leo477831@gmail.com>
2019-01-05 21:03:41 +0800

    Part 1: Preparing layouts

---
+++ app/src/main/java/com/resocoder/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.resocoder.forecastmvvm.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat(){
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+    }
+}
