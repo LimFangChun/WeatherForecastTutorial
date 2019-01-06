9d8ab4cff5570fc4efc6488fbbd12e68e605bd24
LimFangChun <leo477831@gmail.com>
2019-01-06 11:39:30 +0800

    Part 2: Fetch API data with Retrofit, Coroutines, GSON

--- app/src/main/java/com/resocoder/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/resocoder/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
@@ -8,6 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.resocoder.forecastmvvm.R
+import com.resocoder.forecastmvvm.data.ApixuWeatherApiService
+import kotlinx.android.synthetic.main.current_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -26,6 +32,12 @@ class CurrentWeatherFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
+        val apiService = ApixuWeatherApiService()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val currentWeatherResponse = apiService.getCurrentWeather("Kuala Lumpur").await()
+            textView.text = currentWeatherResponse.toString()
+        }
     }
 
 }
