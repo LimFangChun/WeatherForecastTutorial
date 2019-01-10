2e06bfb1ca4f59d5b89f8054c50f0b091f49d1c0
LimFangChun <leo477831@gmail.com>
2019-01-10 11:38:45 +0800

    Part 10: Getting device location

--- app/src/main/java/com/resocoder/forecastmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/resocoder/forecastmvvm/ui/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.resocoder.forecastmvvm.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
 import com.resocoder.forecastmvvm.R
 import kotlinx.android.synthetic.main.activity_main.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
-class MainActivity : AppCompatActivity() {
+private const val PERMISSION_ACCESS_COARSE_LOCATION = 1
+
+class MainActivity : AppCompatActivity(), KodeinAware {
+
+    override val kodein: Kodein by closestKodein()
+    private val fusedLocationProviderClient: FusedLocationProviderClient by instance()
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult?) {
+            super.onLocationResult(p0)
+        }
+    }
 
     private lateinit var navController: NavController
 
@@ -23,9 +46,49 @@ class MainActivity : AppCompatActivity() {
         bottom_nav.setupWithNavController(navController)
 
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        requestLocationPermission()
+
+        if (hasLocationPermission()) {
+            bindLocationManager()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(null, navController)
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                PERMISSION_ACCESS_COARSE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray) {
+        if (requestCode == PERMISSION_ACCESS_COARSE_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                bindLocationManager()
+            } else {
+                Toast.makeText(this@MainActivity,
+                        "Please set location manually in settings", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun bindLocationManager(){
+        LifeCycleBoundLocationManager(
+                this,
+                fusedLocationProviderClient,
+                locationCallback)
     }
 }
